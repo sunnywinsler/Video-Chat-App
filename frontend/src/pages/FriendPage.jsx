b3aad44aa86d459bf3ef875c253d36f10a7bb3b1
sunnywinsler <sunnywinsler@gmail.com>
2025-07-02 18:53:51 +0530

    Initial commit

---
+++ frontend/src/pages/FriendPage.jsx
@@ -0,0 +1,11 @@
+// src/pages/FriendsPage.jsx
+const FriendsPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <h1 className="text-3xl font-bold">Friends</h1>
+      <p className="text-lg mt-2">Your friends will appear here.</p>
+    </div>
+  );
+};
+
+export default FriendsPage;
